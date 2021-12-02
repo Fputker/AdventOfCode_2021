a7d9e095dd31751fcf3f3d39750fda0fd4ad6466
Florian Putker <florian.m.putker@gmail.com>
2021-12-02 18:34:48 +0100

    ugly works

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -29,10 +29,38 @@ fun simpleDirections(){
     println( "the answer is : " + forwardNumbers.sum() * ( downNumbers.sum() + upNumbers.sum()))
 }
 
-fun processDirection(direction: String, position: Triple<Int,Int,Int>): Triple<Int,Int,Int>{
-    return Triple(0,0,0)
+fun getNewPositionUsingForwardDirection(direction: String, position: Triple<Int,Int,Int>): Triple<Int,Int,Int>{
+    val value = directionToNumber(direction)
+    return Triple(position.first+value,position.second,position.third + (position.second * value) )
+}
+
+fun getNewPositionUsingDownDirection(direction: String, position: Triple<Int,Int,Int>): Triple<Int,Int,Int>{
+    val value = directionToNumber(direction)
+    return Triple(position.first,position.second + value,position.third)
+}
+
+fun getNewPositionUsingUpDirection(direction: String, position: Triple<Int,Int,Int>): Triple<Int,Int,Int>{
+    val value = directionToNumber(direction)
+    return Triple(position.first,position.second - value,position.third)
 }
 
 fun main() {
-    simpleDirections()
+    val directions =
+        readInputIntoListOfString("/Users/fputker/Ideaprojects/AdventOfCode_2021/src/main/kotlin/input/day2.txt")
+    var position: Triple<Int,Int,Int> = Triple(0,0,0)
+    for (direction in directions){
+        when(direction[0]){
+            'f' -> {position = getNewPositionUsingForwardDirection(direction, position)}
+            'd' -> {position = getNewPositionUsingDownDirection(direction, position)}
+            'u' -> {position = getNewPositionUsingUpDirection(direction, position)}
+        }
+    }
+    println("The final answer is: " + position.first * position.third)
+
+}
+
+class Position(triple: Triple<Int, Int, Int>){
+    val horizontal: Int = triple.first
+    val aim: Int = triple.second
+    val depth: Int = triple.third
 }
--- src/test/kotlin/Day2KtTest.kt
+++ src/test/kotlin/Day2KtTest.kt
@@ -1,12 +1,43 @@
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
-
 internal class Day2KtTest {
 
     @Test
     fun directionToNumber() {
         val direction = "forward 1"
         assert(directionToNumber(direction) == 1)
+    }
+
+    @Test
+    fun `Given Direction forward5 when processDirection get triple(5,0,0)`() {
+        val input: MutableList<String> = mutableListOf("forward 5","down 5","forward 8","up 3", "down 8", "forward 2")
+        val newPosition = getNewPositionUsingForwardDirection(input[0], Triple(0,0,0))
+        assert(newPosition.first == 5)
+        assert(newPosition.second == 0)
+        assert(newPosition.third == 0)
+    }
+
+    @Test
+    fun `Given Direction down5 when processDirection with triple(5,0,0) get triple(5,5,0)`() {
+        val input: MutableList<String> = mutableListOf("forward 5","down 5","forward 8","up 3", "down 8", "forward 2")
+        val newPosition = getNewPositionUsingForwardDirection(input[0], Triple(0,0,0))
+        val newerPosition = getNewPositionUsingDownDirection(input[1], newPosition)
+        assert(newerPosition.first == 5)
+        assert(newerPosition.second == 5)
+        assert(newerPosition.third == 0)
+    }
+
+    @Test
+    fun `Given Directions when processDirection with triple(5,0,0) get triple(5,5,0)`() {
+        val input: MutableList<String> = mutableListOf("forward 5","down 5","forward 8","up 3", "down 8", "forward 2")
+        var position: Triple<Int,Int,Int> = Triple(0,0,0)
+        for (direction in input){
+            when(direction[0]){
+                'f' -> {position = getNewPositionUsingForwardDirection(direction, position)}
+                'd' -> {position = getNewPositionUsingDownDirection(direction, position)}
+                'u' -> {position = getNewPositionUsingUpDirection(direction, position)}
+            }
+        }
+        assert ((position.first * position.third) == 900)
     }
 }
