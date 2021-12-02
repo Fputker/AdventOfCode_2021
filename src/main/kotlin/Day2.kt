af5291a4d5ff723ce3feec390cc388d1a193a535
Florian Putker <florian.m.putker@gmail.com>
2021-12-02 20:49:27 +0100

    try Day2 part 1 with map and fold

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.lang.Exception
 
 fun readInputIntoListOfString(filePath: String): List<String> {
     val input: MutableList<String> = mutableListOf()
@@ -11,56 +12,58 @@ fun parseInstruction(instruction: String): Pair<String, Int> {
     return Pair(direction, quantity.toInt())
 }
 
-fun simpleinstructions(){
+fun executeInstruction(instruction: Pair<String, Int>): Pair<Int, Int> {
+    return when (instruction.first) {
+        "forward" -> instruction.second to 0
+        "down" -> 0 to instruction.second
+        "up" -> 0 to -instruction.second
+        else -> throw Exception("unknown instruction")
+    }
+}
+
+fun processPartOneInstructions() {
     val instructions =
         readInputIntoListOfString("/Users/fputker/Ideaprojects/AdventOfCode_2021/src/main/kotlin/input/day2.txt")
-
-    val forward = instructions.filter { it.contains("forward") }
-    val down = instructions.filter { it.contains("down") }
-    val up = instructions.filter { it.contains("up") }
-
-    val forwardNumbers: MutableList<Int> = mutableListOf()
-    val downNumbers: MutableList<Int> = mutableListOf()
-    val upNumbers: MutableList<Int> = mutableListOf()
-
-    forward.forEach { forwardNumbers.add(parseInstruction(it).second) }
-    down.forEach { downNumbers.add(parseInstruction(it).second) }
-    up.forEach { upNumbers.add(parseInstruction(it).second * -1)  }
-
-    println( "the answer is : " + forwardNumbers.sum() * ( downNumbers.sum() + upNumbers.sum()))
+    val output = instructions.map { parseInstruction(it) }
+        .fold(0 to 0) { position, instruction ->
+            position.first + executeInstruction(instruction).first to
+                    position.second + executeInstruction(instruction).second
+        }
+    println("The final position = " + output.first * output.second)
 }
 
-fun getNewPositionUsingForwardinstruction(instruction: String, position: Triple<Int,Int,Int>): Triple<Int,Int,Int>{
+fun getNewPositionUsingForwardinstruction(instruction: String, position: Triple<Int, Int, Int>): Triple<Int, Int, Int> {
     val value = parseInstruction(instruction).second
-    return Triple(position.first+value,position.second,position.third + (position.second * value) )
+    return Triple(position.first + value, position.second, position.third + (position.second * value))
 }
 
-fun getNewPositionUsingDowninstruction(instruction: String, position: Triple<Int,Int,Int>): Triple<Int,Int,Int>{
+fun getNewPositionUsingDowninstruction(instruction: String, position: Triple<Int, Int, Int>): Triple<Int, Int, Int> {
     val value = parseInstruction(instruction).second
-    return Triple(position.first,position.second + value,position.third)
+    return Triple(position.first, position.second + value, position.third)
 }
 
-fun getNewPositionUsingUpinstruction(instruction: String, position: Triple<Int,Int,Int>): Triple<Int,Int,Int>{
+fun getNewPositionUsingUpinstruction(instruction: String, position: Triple<Int, Int, Int>): Triple<Int, Int, Int> {
     val value = parseInstruction(instruction).second
-    return Triple(position.first,position.second - value,position.third)
+    return Triple(position.first, position.second - value, position.third)
 }
 
 fun main() {
-    val instructions =
-        readInputIntoListOfString("/Users/fputker/Ideaprojects/AdventOfCode_2021/src/main/kotlin/input/day2.txt")
-    var position: Triple<Int,Int,Int> = Triple(0,0,0)
-    for (instruction in instructions){
-        when(instruction[0]){
-            'f' -> {position = getNewPositionUsingForwardinstruction(instruction, position)}
-            'd' -> {position = getNewPositionUsingDowninstruction(instruction, position)}
-            'u' -> {position = getNewPositionUsingUpinstruction(instruction, position)}
-        }
-    }
-    println("The final answer is: " + position.first * position.third)
+    processPartOneInstructions()
+//    val instructions =
+//        readInputIntoListOfString("/Users/fputker/Ideaprojects/AdventOfCode_2021/src/main/kotlin/input/day2.txt")
+//    var position: Triple<Int,Int,Int> = Triple(0,0,0)
+//    for (instruction in instructions){
+//        when(instruction[0]){
+//            'f' -> {position = getNewPositionUsingForwardinstruction(instruction, position)}
+//            'd' -> {position = getNewPositionUsingDowninstruction(instruction, position)}
+//            'u' -> {position = getNewPositionUsingUpinstruction(instruction, position)}
+//        }
+//    }
+//    println("The final answer is: " + position.first * position.third)
 
 }
 
-class Position(triple: Triple<Int, Int, Int>){
+class Position(triple: Triple<Int, Int, Int>) {
     val horizontal: Int = triple.first
     val aim: Int = triple.second
     val depth: Int = triple.third
