23f777b816d8a319e94a02be820ef225b7fd50f9
Florian Putker <florian.m.putker@gmail.com>
2021-12-02 21:25:25 +0100

    refactor part 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -24,41 +24,25 @@ fun processPartOneInstructions() {
     println("The final position of part 1 = " + output.first * output.second)
 }
 
+fun executePartTwoInstruction(instruction: Pair<String, Int>, position: Triple<Int, Int, Int>): Triple<Int, Int, Int> {
+    return when (instruction.first) {
+        "forward" -> Triple(position.first + instruction.second, position.second, position.third + (position.second * instruction.second))
+        "down" -> Triple(position.first, position.second + instruction.second, position.third)
+        "up" -> Triple(position.first, position.second - instruction.second, position.third)
+        else -> throw Exception("unknown instruction")
+    }
+}
+
 fun processPartTwoInstructions(){
     val instructions = readInput("day2")
-    var position: Triple<Int,Int,Int> = Triple(0,0,0)
-    for (instruction in instructions){
-        when(instruction[0]){
-            'f' -> {position = getNewPositionUsingForwardinstruction(instruction, position)}
-            'd' -> {position = getNewPositionUsingDowninstruction(instruction, position)}
-            'u' -> {position = getNewPositionUsingUpinstruction(instruction, position)}
+    val result = instructions.map { parseInstruction(it) }
+        .fold(Triple(0,0,0)) {
+            position, instruction -> executePartTwoInstruction(instruction, position)
         }
-    }
-    println("The final position of part 2 = " + position.first * position.third)
-}
-
-fun getNewPositionUsingForwardinstruction(instruction: String, position: Triple<Int, Int, Int>): Triple<Int, Int, Int> {
-    val value = parseInstruction(instruction).second
-    return Triple(position.first + value, position.second, position.third + (position.second * value))
-}
-
-fun getNewPositionUsingDowninstruction(instruction: String, position: Triple<Int, Int, Int>): Triple<Int, Int, Int> {
-    val value = parseInstruction(instruction).second
-    return Triple(position.first, position.second + value, position.third)
-}
-
-fun getNewPositionUsingUpinstruction(instruction: String, position: Triple<Int, Int, Int>): Triple<Int, Int, Int> {
-    val value = parseInstruction(instruction).second
-    return Triple(position.first, position.second - value, position.third)
+    println("The final position of part 1 = " + result.first * result.third)
 }
 
 fun main() {
     processPartOneInstructions()
     processPartTwoInstructions()
-}
-
-class Position(triple: Triple<Int, Int, Int>) {
-    val horizontal: Int = triple.first
-    val aim: Int = triple.second
-    val depth: Int = triple.third
 }
--- src/test/kotlin/Day2KtTest.kt
+++ src/test/kotlin/Day2KtTest.kt
@@ -11,7 +11,8 @@ internal class Day2KtTest {
     @Test
     fun `Given instruction forward5 when processinstruction get triple(5,0,0)`() {
         val input: MutableList<String> = mutableListOf("forward 5","down 5","forward 8","up 3", "down 8", "forward 2")
-        val newPosition = getNewPositionUsingForwardinstruction(input[0], Triple(0,0,0))
+        val parsedInstructions = input.map { parseInstruction(it) }
+        val newPosition = executePartTwoInstruction(parsedInstructions[0], Triple(0,0,0))
         assert(newPosition.first == 5)
         assert(newPosition.second == 0)
         assert(newPosition.third == 0)
@@ -20,8 +21,10 @@ internal class Day2KtTest {
     @Test
     fun `Given instruction down5 when processinstruction with triple(5,0,0) get triple(5,5,0)`() {
         val input: MutableList<String> = mutableListOf("forward 5","down 5","forward 8","up 3", "down 8", "forward 2")
-        val newPosition = getNewPositionUsingForwardinstruction(input[0], Triple(0,0,0))
-        val newerPosition = getNewPositionUsingDowninstruction(input[1], newPosition)
+        val parsedInstructions = input.map { parseInstruction(it) }
+
+        val newPosition = executePartTwoInstruction(parsedInstructions[0], Triple(0,0,0))
+        val newerPosition = executePartTwoInstruction(parsedInstructions[1], newPosition)
         assert(newerPosition.first == 5)
         assert(newerPosition.second == 5)
         assert(newerPosition.third == 0)
@@ -30,14 +33,10 @@ internal class Day2KtTest {
     @Test
     fun `Given instructions when processinstruction with triple(5,0,0) get triple(5,5,0)`() {
         val input: MutableList<String> = mutableListOf("forward 5","down 5","forward 8","up 3", "down 8", "forward 2")
-        var position: Triple<Int,Int,Int> = Triple(0,0,0)
-        for (instruction in input){
-            when(instruction[0]){
-                'f' -> {position = getNewPositionUsingForwardinstruction(instruction, position)}
-                'd' -> {position = getNewPositionUsingDowninstruction(instruction, position)}
-                'u' -> {position = getNewPositionUsingUpinstruction(instruction, position)}
+        val result = input.map { parseInstruction(it) }
+            .fold(Triple(0,0,0)) {
+                    position, instruction -> executePartTwoInstruction(instruction, position)
             }
-        }
-        assert ((position.first * position.third) == 900)
+        assert ((result.first * result.third) == 900)
     }
 }
