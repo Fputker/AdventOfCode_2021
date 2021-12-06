a0b31046d60ffdb6b50f653b268640da75e69b7f
Florian Putker <florian.m.putker@gmail.com>
2021-12-06 20:03:44 +0100

    better fishlist

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -2,32 +2,56 @@ fun createFishList(input: String): MutableList<Int> {
     return input.split(",").map { it.toInt() } as MutableList<Int>
 }
 
-fun day6part1(){
+fun day6part1() {
     val startingFish = createFishList(readInput("Day6")[0])
     println(ageXamountOfDays(80, startingFish).size)
 }
 
-fun ageXamountOfDays(nrOfDays: Int, startingFish: MutableList<Int>): List<Int>{
+fun ageXamountOfDays(nrOfDays: Int, startingFish: MutableList<Int>): List<Int> {
     var copy: MutableList<Int> = startingFish
-    for (day in 1..nrOfDays){
+    for (day in 1..nrOfDays) {
         copy = ageOneDay(copy)
-        println(copy )
+        println(copy)
     }
     return copy
 }
 
-fun ageOneDay(listOfFish: MutableList<Int>): MutableList<Int>{
+fun ageOneDay(listOfFish: MutableList<Int>): MutableList<Int> {
     val aged = listOfFish.map { it - 1 } as MutableList<Int>
     return processAgedFish(aged)
 }
 
-fun processAgedFish(listOfFish : MutableList<Int>): MutableList<Int> {
+fun processAgedFish(listOfFish: MutableList<Int>): MutableList<Int> {
     var newlistOfFish = listOfFish
-    for (i in 1..listOfFish.count { it == -1 }){
+    for (i in 1..listOfFish.count { it == -1 }) {
         newlistOfFish.add(8)
     }
-    newlistOfFish = newlistOfFish.map { if(it==-1) 6 else it } as MutableList<Int>
+    newlistOfFish = newlistOfFish.map { if (it == -1) 6 else it } as MutableList<Int>
     return newlistOfFish
+}
+
+fun createNewFishList(input: String): List<Int> {
+    val inputInts = input.split(",").map { it.toInt() }
+    val fishlist = listOf(
+        inputInts.count { it == 0 },
+        inputInts.count { it == 1 },
+        inputInts.count { it == 2 },
+        inputInts.count { it == 3 },
+        inputInts.count { it == 4 },
+        inputInts.count { it == 5 },
+        inputInts.count { it == 6 },
+        inputInts.count { it == 7 },
+        inputInts.count { it == 8 })
+    return fishlist
+}
+
+fun ageAday(fishList: MutableList<Int>): List<Int> {
+    val fishAt0 = fishList[0]
+    for (i in 0..fishList.size-2) {
+        fishList[i] = fishList[i+1]
+    }
+    fishList[8]=fishAt0
+    return fishList
 }
 
 fun main() {
--- src/test/kotlin/Day6KtTest.kt
+++ src/test/kotlin/Day6KtTest.kt
@@ -4,39 +4,62 @@ import kotlin.test.assertEquals
 internal class Day6KtTest {
 
     @Test
-    fun `test simple aging`(){
-        val input = mutableListOf(3,4,3,1,2)
-        val expect = mutableListOf(2,3,2,0,1)
+    fun `test simple aging`() {
+        val input = mutableListOf(3, 4, 3, 1, 2)
+        val expect = mutableListOf(2, 3, 2, 0, 1)
         assertEquals(expect, ageOneDay(input))
     }
 
     @Test
-    fun `test spawning fish`(){
-        val input = mutableListOf(2,3,2,0,1)
-        val expect = mutableListOf(1,2,1,6,0,8)
+    fun `test spawning fish`() {
+        val input = mutableListOf(2, 3, 2, 0, 1)
+        val expect = mutableListOf(1, 2, 1, 6, 0, 8)
         assertEquals(expect, ageOneDay(input))
     }
 
     @Test
     fun `test complete input`() {
-        val input = mutableListOf(3,4,3,1,2)
+        val input = mutableListOf(3, 4, 3, 1, 2)
 
-        val expect = mutableListOf(6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8)
+        val expect = mutableListOf(6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8)
         assertEquals(ageXamountOfDays(18, input), expect)
     }
 
     @Test
     fun `test complete input for 80 days`() {
-        val input = mutableListOf(3,4,3,1,2)
+        val input = mutableListOf(3, 4, 3, 1, 2)
 
         assertEquals(ageXamountOfDays(80, input).size, 5934)
     }
 
     @Test
     fun `test complete input for 256 days`() {
-        val input = mutableListOf(3,4,3,1,2)
+        val input = mutableListOf(3, 4, 3, 1, 2)
 
         assertEquals(ageXamountOfDays(80, input).size, 5934)
+    }
+
+    @Test
+    fun `test initialization`() {
+        val input = "3,4,3,1,2"
+
+        val expect = listOf(0, 1, 1, 2, 1, 0, 0, 0, 0)
+
+        assertEquals(expect, createNewFishList(input))
+    }
+
+    @Test
+    fun `test simple aging again`() {
+        val input = mutableListOf(0, 1, 1, 2, 1,0,0,0,0)
+        val expect = mutableListOf(1, 1, 2, 1, 0,0,0,0,0)
+        assertEquals(expect, ageAday(input))
+    }
+
+    @Test
+    fun `test simple aging again and again`() {
+        val input = mutableListOf(1, 1, 2, 1, 0,0,0,0,0)
+        val expect = mutableListOf(1, 2, 1, 0, 0,0,0,0,1)
+        assertEquals(expect, ageAday(input))
     }
 
 }
