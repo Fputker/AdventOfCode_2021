42937a57a279d5b84b4dc4f46cb8a09535ce1d82
Florian Putker <florian.m.putker@gmail.com>
2021-12-08 22:00:00 +0100

    Unsightly but working

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -15,25 +15,108 @@ fun countOneFourEightNine(input: List<String>): Int {
 
 fun parseInput(input: List<String>): List<String> {
     val listOflistOfSIgnalOutput = input.map { it.split("|") }
-    println(listOflistOfSIgnalOutput)
     val listOfOutput = listOflistOfSIgnalOutput.map { it[1].trim() }
-    println(listOfOutput)
     val listOfsplitOutput = listOfOutput.map { it.split(" ") }
-    println(listOfsplitOutput)
     val oneList = listOfsplitOutput.flatMap { it }
-    println(oneList)
     return oneList
 }
 
-fun Day8part1(){
+fun Day8part1() {
     val input = readInput("Day8")
     println("answer to part 1 = : ${countOneFourEightNine(input)}")
 }
 
-fun getSegmentMapping(signalList: List<String>): List<String>{
-    return
+fun getSegmentMapping(signalList: List<String>): List<Pair<Int, String>> {
+    var mapOfBitsToNumbers = mutableListOf(
+        Pair(1, signalList.filter { it.length == 2 }[0]),
+        Pair(4, signalList.filter { it.length == 4 }[0]),
+        Pair(7, signalList.filter { it.length == 3 }[0]),
+        Pair(8, signalList.filter { it.length == 7 }[0])
+    )
+
+    val segmentsAsChars = signalList.map { it.toList() }.flatMap { it }
+    val segmentCounts = segmentsAsChars.map { char -> Pair(char, segmentsAsChars.count { it == char }) }.distinct()
+
+    var mapOfBitsToSegments = mutableListOf(
+        Pair("c", segmentCounts.find { it.second == 9 }?.first.toString()),
+        Pair("e", segmentCounts.find { it.second == 4 }?.first.toString()),
+        Pair("f", segmentCounts.find { it.second == 6 }?.first.toString())
+    )
+
+    // add bit 1 to bits to segments
+    mapOfBitsToSegments.add(Pair("a", subtractString(mapOfBitsToNumbers.filter { (it.first == 7) }[0].second, mapOfBitsToNumbers.filter { (it.first == 1) }[0].second)))
+
+    mapOfBitsToSegments.add(Pair("b", subtractString(mapOfBitsToNumbers.filter { (it.first == 1) }[0].second, mapOfBitsToSegments.filter { it.first == "c" }[0].second)))
+
+    mapOfBitsToNumbers.add(Pair(2, subtractString(getBitsForNumber(8, mapOfBitsToNumbers), "${getBitBySegment("c", mapOfBitsToSegments)}${getBitBySegment("f", mapOfBitsToSegments)}")))
+
+    mapOfBitsToNumbers.add(Pair(9, subtractString(getBitsForNumber(8, mapOfBitsToNumbers), getBitBySegment("e", mapOfBitsToSegments))))
+
+    mapOfBitsToNumbers.add(Pair(6, subtractString(getBitsForNumber(8, mapOfBitsToNumbers), getBitBySegment("b", mapOfBitsToSegments))))
+
+    mapOfBitsToNumbers.add(Pair(3, subtractString(getBitsForNumber(8, mapOfBitsToNumbers), getBitBySegment("e", mapOfBitsToSegments) + getBitBySegment("f", mapOfBitsToSegments))))
+
+    mapOfBitsToNumbers.add(Pair(5, subtractString(getBitsForNumber(8, mapOfBitsToNumbers), getBitBySegment("b", mapOfBitsToSegments) + getBitBySegment("e", mapOfBitsToSegments))))
+
+    val orderedSignalList = signalList.map { it.orderString() }.sorted()
+
+    val definedSignals = mapOfBitsToNumbers.map { it.second.orderString() }.sorted()
+
+    val bitsFor0 = orderedSignalList.filterNot { definedSignals.indexOf(it) > -1 }[0]
+
+    mapOfBitsToNumbers.add(Pair(0, bitsFor0))
+
+
+    val sorted = mapOfBitsToNumbers.sortedBy { it.first }
+    println("sorted = ${sorted}")
+    val sortedAgain = sorted.map {
+        Pair(it.first, it.second.toCharArray().sorted().joinToString(""))
+    }
+    return sortedAgain
+}
+
+fun getBitsForNumber(number: Int, mapOfBitsToNumbers: List<Pair<Int, String>>): String {
+    val a = mapOfBitsToNumbers.filter { it.first == number }[0].second
+    return a
+}
+
+fun getBitBySegment(segment: String, mapOfBitsToSegments: List<Pair<String, String>>): String {
+    val a = mapOfBitsToSegments.filter { it.first == segment }[0].second
+    return a
+}
+
+fun String.orderString(): String {
+    return this.toCharArray().sorted().joinToString("")
+}
+
+fun subtractString(s1: String, s2: String): String {
+    val a = s1  // "abc"
+    val b = s2 //"cb"
+    return s1.filterNot { s2.indexOf(it) > -1 }
+}
+
+fun findNumberForSegment(segment: String, segmentMapping: List<Pair<Int, String>>): Int {
+    val match = segmentMapping.filter { it.second == segment.orderString() }
+    return match[0].first
+}
+
+fun Day8part2() {
+    val input = readInput("Day8")
+    println("answer to part 2 = ${input.map { findAnswerForSingleLine(it) }.sum()}")
+}
+
+fun findAnswerForSingleLine(s: String): Int {
+    val listOfSignalWithAnswer = s.split("|").map { it.trim() }
+    val segmentMap = getSegmentMapping(listOfSignalWithAnswer[0].split(" "))
+    val answer = listOfSignalWithAnswer[1]
+        .split(" ")
+        .map { findNumberForSegment(it, segmentMap) }
+        .joinToString("")
+        .toInt()
+    return answer
 }
 
 fun main() {
     Day8part1()
+    Day8part2()
 }
--- src/test/kotlin/Day8KtTest.kt
+++ src/test/kotlin/Day8KtTest.kt
@@ -1,34 +1,89 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-internal class Day8KtTest{
+internal class Day8KtTest {
 
     @Test
-    fun testCounting(){
+    fun testCounting() {
         val input = listOf("""be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |
         fdgacbe cefdb cefbgd gcbe""",
-               """ edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |
+            """ edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |
         fcgedb cgb dgebacf gc""",
-               """ fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |
+            """ fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |
         cg cg fdcagb cbg""",
-               """ fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |
+            """ fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |
         efabcd cedba gadfec cb""",
-               """ aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |
+            """ aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |
         gecf egdcabf bgf bfgea""",
-               """ fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |
+            """ fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |
         gebdcfa ecba ca fadegcb""",
-               """ dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |
+            """ dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |
         cefg dcbef fcge gbcadfe""",
-               """ bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |
+            """ bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |
         ed bcgafe cdgba cbgef""",
-               """ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |
+            """ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |
         gbdfcae bgc cg cgb""",
-               """ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |
+            """ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |
         fgae cfgab fg bagce """)
 
         assertEquals(26, countOneFourEightNine(input))
 
     }
 
+    @Test
+    fun `test subtractString`() {
+        val a = "abc"
+        val b = "cb"
+        assertEquals("a", subtractString(a, b))
+    }
+
+    @Test
+    fun `test getSegmentMapping`() {
+        val signalList = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab".split(" ")
+        val expected = listOf<Pair<Char, Int>>()
+        assertEquals(expected, getSegmentMapping(signalList))
+    }
+
+    @Test
+    fun `test part2 `() {
+        val signalList = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab".split(" ")
+        println(signalList)
+        val expected = listOf(
+            Pair(8, "acedgfb"),
+            Pair(5, "cdfbe"),
+            Pair(2, "gcdfa"),
+            Pair(3, "fbcad"),
+            Pair(7, "dab"),
+            Pair(9, "cefabd"),
+            Pair(6, "cdfgeb"),
+            Pair(4, "eafb"),
+            Pair(0, "cagedb"),
+            Pair(1, "ab"),
+        )
+        val result = getSegmentMapping(signalList)
+        println("result=$result")
+        assert(expected.sortedBy { it.first }.equals(result))
+    }
+
+    @Test
+    fun `test find number for segment`(){
+        val input = "cdfeb"
+        val signalList = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab".split(" ")
+
+        val segmentMapping = getSegmentMapping(signalList)
+
+        assertEquals(5, findNumberForSegment(input, segmentMapping))
+    }
+
+    @Test
+    fun testfindAnswerForSingleLine(){
+        val input = """acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |
+        cdfeb fcadb cdfeb cdbaf"""
+
+        val expected = 5353
+
+        assertEquals(expected, findAnswerForSingleLine(input))
+
+    }
 
 }
