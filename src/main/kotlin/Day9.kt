118469f10fbfd0cfafea5687b80ddece1524cbad
fputker <fputker@bol.com>
2021-12-10 06:34:15 +0100

    da2 2 partial

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,5 +1,3 @@
-
-
 fun createCoordinateListOfXYlowpoints(heightMap: List<String>): List<Coordinate> {
     val newMap = heightMap.map { string -> string.toCharArray().map { it.digitToInt() } }
     var coordinates = mutableListOf<Coordinate>()
@@ -31,30 +29,120 @@ fun findLowPointsInLine(newMap: List<List<Int>>, i: Int): MutableList<Coordinate
 }
 
 
+data class Coordinate(val X: Int, val Y: Int, val height: Int, val left: Int? = null, val right: Int? = null) {
 
-data class Coordinate(val X: Int, val Y: Int, val height: Int, val left: Int? = null, val right: Int? = null, ) {
-    fun determineIfUpDownLowpoint(heightMap: List<List<Int>>): Boolean{
+    fun determineIfUpDownLowpoint(heightMap: List<List<Int>>): Boolean {
         if (Y != 0 && (Y + 1) <= heightMap.size - 1) { // check whole lines except first and last digit
-            if (height < heightMap[Y-1][X]  && height < heightMap[Y+1][X]) {
+            if (height < heightMap[Y - 1][X] && height < heightMap[Y + 1][X]) {
                 return true
             }
         } else if (Y == 0) {
-            if (height < heightMap[Y+1][X]) {
+            if (height < heightMap[Y + 1][X]) {
                 return true
             }
         } else if ((Y + 1) > heightMap.size - 1) {
-            if (height < heightMap[Y-1][X]) {
+            if (height < heightMap[Y - 1][X]) {
                 return true
             }
         }
         return false
+    }
+
+    fun leftIslarger() = left!! > height && left != 9
+
+    fun rightIslarger() = right!! > height && right != 9
+
+    fun topIsLarger(heightMap: List<List<Int>>) = if (Y>0) {heightMap[Y-1][X] > height && heightMap[Y-1][X] != 9} else false
+
+    fun bottomIsLarger(heightMap: List<List<Int>>) = if (Y < heightMap.size) {heightMap[Y+1][X] > height && heightMap[Y+1][X] != 9} else false
+
+    fun findLeftBasinCoordinates(heightMap: List<List<Int>>, coordinates: MutableList<Coordinate>): List<Coordinate> {
+        val newCoordinates = coordinates
+        coordinates.forEach {
+            if (it.leftIslarger()) {
+                val newCoordinate = it.left?.let { it1 -> Coordinate(X = it.X - 1, Y = it.Y, height = it1, left = heightMap[it.Y][it.X - 1], right = it.height) }
+                if (newCoordinate != null) {
+                    newCoordinates.add(newCoordinate)
+                }
+                if (newCoordinates.equals(coordinates)) {
+                    return coordinates
+                } else {
+                    this.findLeftBasinCoordinates(heightMap, newCoordinates)
+                }
+            }
+        }
+        return coordinates
+    }
+
+    fun findRightBasinCoordinates(heightMap: List<List<Int>>, coordinates: MutableList<Coordinate>): List<Coordinate> {
+        val newCoordinates = coordinates
+        coordinates.forEach {
+            if (it.rightIslarger()) {
+                val newCoordinate = it.right?.let { it1 -> Coordinate(X = it.X + 1, Y = it.Y, height = it1, left = heightMap[it.Y][it.X + 1], right = it.height) }
+                if (newCoordinate != null) {
+                    newCoordinates.add(newCoordinate)
+                }
+                if (newCoordinates.equals(coordinates)) {
+                    return coordinates
+                } else {
+                    this.findRightBasinCoordinates(heightMap, newCoordinates)
+                }
+            }
+        }
+        return coordinates
+    }
+
+    fun findTopBasinCoordinates(heightMap: List<List<Int>>, coordinates: MutableList<Coordinate>): List<Coordinate> {
+        val newCoordinates = coordinates
+        coordinates.forEach {
+            if (it.topIsLarger(heightMap)) {
+                val newCoordinate = Coordinate(X = it.X, Y = it.Y-1, height = heightMap[Y-1][X], left = heightMap[it.Y-1][it.X-1], right = heightMap[it.Y-1][it.X+1])
+                newCoordinates.add(newCoordinate)
+                if (newCoordinates.equals(coordinates)) {
+                    return coordinates
+                } else {
+                    this.findTopBasinCoordinates(heightMap, newCoordinates)
+                }
+            }
+        }
+        return coordinates
+    }
+
+    fun findBottomBasinCoordinates(heightMap: List<List<Int>>, coordinates: MutableList<Coordinate>): List<Coordinate> {
+        val newCoordinates = coordinates
+        coordinates.forEach {
+            if (it.bottomIsLarger(heightMap)) {
+                val newCoordinate = Coordinate(X = it.X, Y = it.Y+1, height = heightMap[Y+1][X], left = heightMap[it.Y+1][it.X-1], right = heightMap[it.Y+1][it.X+1])
+                newCoordinates.add(newCoordinate)
+                if (newCoordinates.equals(coordinates)) {
+                    return coordinates
+                } else {
+                    this.findBottomBasinCoordinates(heightMap, newCoordinates)
+                }
+            }
+        }
+        return coordinates
+    }
+
+
+
+    fun findVerticalBasinCoordinates(): List<Coordinate> {
+        return listOf()
+    }
+}
+
+data class Basin(var coordinates: List<Coordinate>) {
+    val id = this.hashCode()
+
+    fun mapCompleteBasin(heightMap: List<List<Int>>, coordinates: MutableList<Coordinate>) {
+
     }
 }
 
 fun main() {
     val input = readInput("day9")
     val lowpoints = createCoordinateListOfXYlowpoints(input)
-    val lowpointHeights = lowpoints.map{it.height + 1}
+    val lowpointHeights = lowpoints.map { it.height + 1 }
     println("part1 = ${lowpointHeights.sum()}")
 
 
--- src/test/kotlin/Day9KtTest.kt
+++ src/test/kotlin/Day9KtTest.kt
@@ -26,4 +26,45 @@ internal class Day9KtTest {
 
         assertEquals(2, findLowPointsInLine(newMap, 0 ).size)
     }
+
+    @Test
+    fun `test findHorizontalBasinCoordinates`(){
+        val testInput = listOf(
+            "2199943210"
+        )
+
+        val newMap = testInput.map { string -> string.toCharArray().map { it.digitToInt() } }
+        val startList = mutableListOf(Coordinate(1,0,1, 2, 9))
+
+        assertEquals(2, Coordinate(1,0,1, 2, 9).findLeftBasinCoordinates(newMap, startList).size)
+    }
+
+    @Test
+    fun `test findrightBasinCoordinates`(){
+        val testInput = listOf(
+            "2199943210"
+        )
+
+        val newMap = testInput.map { string -> string.toCharArray().map { it.digitToInt() } }
+        val startList = mutableListOf(Coordinate(1,0,1, 2, 9))
+
+        assertEquals(1, Coordinate(1,0,1, 2, 9).findRightBasinCoordinates(newMap, startList).size)
+    }
+
+    @Test
+    fun `test findrTopBasinCoordinates`(){
+        val testInput = listOf(
+            "2199943210",
+            "3987894921",
+            "9856789892",
+            "8767896789",
+            "9899965678"
+        )
+
+        val newMap = testInput.map { string -> string.toCharArray().map { it.digitToInt() } }
+        val startList = mutableListOf(Coordinate(1,0,1, 2, 9))
+
+        assertEquals(1, Coordinate(1,0,1, 2, 9).findRightBasinCoordinates(newMap, startList).size)
+    }
+
 }
